f5a620b630ad142084d672b536427d58f277b6fa
Coltea <coltea@qq.com>
2021-03-22 17:58:43 +0800

    first commit

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ async def read_own_items():
 
 
 if __name__ == '__main__':
-    uvicorn.run(app='run:app', host="127.0.0.1", port=8000, reload=True, debug=True)
+    uvicorn.run(app='run:app', host="0.0.0.0", port=80, reload=True, debug=True)
